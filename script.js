b6d840d52a8313515474a81f9b93273f141a285e
Baloyi Muhluri <accemhluri@gmail.com>
2026-07-27 09:23:08 +0200

    added each product's page.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const product1 = document.getElementById("prd1");
+const product2 = document.getElementById("prd2");
+const product3 = document.getElementById("prd3");
+const product4 = document.getElementById("prd4");
+const product5 = document.getElementById("prd5");
 
 product1.addEventListener("click", function () {
-    window.location.href = "about_wahl.html";
+    window.location.href = "products/about_wahl.html";
+})
+
+product2.addEventListener("click", function () {
+    window.location.href = "products/sponge.html";
+})
+
+product3.addEventListener("click", function () {
+    window.location.href = "products/spray.html";
+})
+
+product4.addEventListener("click", function () {
+    window.location.href = "products/durag.html";
+})
+
+product5.addEventListener("click", function () {
+    window.location.href = "products/combs.html";
 })

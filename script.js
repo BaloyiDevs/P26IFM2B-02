ce3300c5c73d5b74a0d1fef2a70ef3e8b2074db4
Baloyi Muhluri <accemhluri@gmail.com>
2026-07-28 20:44:40 +0200

    renamed the product clippers, added an unordered and ordered list to each product page.

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const product2 = document.getElementById("prd2");
 const product3 = document.getElementById("prd3");
 const product4 = document.getElementById("prd4");
 const product5 = document.getElementById("prd5");
+const product6 = document.getElementById("prd6");
 
 product1.addEventListener("click", function () {
-    window.location.href = "products/about_wahl.html";
+    window.location.href = "products/clippers.html";
 })
 
 product2.addEventListener("click", function () {
@@ -22,4 +23,8 @@ product4.addEventListener("click", function () {
 
 product5.addEventListener("click", function () {
     window.location.href = "products/combs.html";
+})
+
+product6.addEventListener("click", function () {
+    window.location.href = "products/brushes.html";
 })
